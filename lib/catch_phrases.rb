99a7a0078c505bb1c8204760de8761d6d9c94bf9
Ryan Anderson <ryan.t.anderson19@gmail.com>
2020-03-07 04:34:01 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -14,9 +14,9 @@ phrase = "It's Dangerous To Go Alone! Take This."
 puts phrase
 end
 
-describe "all_phrases" do
-  it "takes in an argument and puts out the catch phrase" do
-    expect{all_phrases}.to output(/It's-a me, Mario!\n/).to_stdout
+def all_phrases
+  do it "takes in an argument and puts out the catch phrase" do
+    phrase = 
     expect{all_phrases}.to output(/Thank You Mario! But Our Princess Is In Another Castle!\n/).to_stdout
     expect{all_phrases}.to output(/It's Dangerous To Go Alone! Take This.\n/).to_stdout
   end
