be7b581069b55c757203de126aca565b70aa16da
Ryan Anderson <ryan.t.anderson19@gmail.com>
2020-03-07 04:39:33 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -17,7 +17,9 @@ end
 def all_phrases
     phrase1 = "Thank You Mario! But Our Princess Is In Another Castle!"
     phrase2 = "It's Dangerous To Go Alone! Take This."
-    phrase3 = 
-  end
+    phrase3 = "It's-a me, Mario!"
+puts phrase1
+puts phrase2
+puts phrase3
 end
   
